dbf763f26fe1b40b1be9a8ca288baa7f6e096abe
Isaac Fernando <isaacfernandonovo@gmail.com>
2023-05-23 14:46:57 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,13 @@
+function abrir_pop() {
+    let fundo = document.getElementById("esconder");
+    {if (fundo.style.display = "none");
+        fundo.style.display = 'flex';
+    }
+}
+
+function fechar_pop() {
+    let fundo = document.getElementById("esconder");
+    {if (fundo.style.display = "flex");
+        fundo.style.display = 'none';
+    }
+}
