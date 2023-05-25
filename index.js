e282617da4b49a229016daae16aef8b44f53227c
Isaac Fernando <isaacfernandonovo@gmail.com>
2023-05-24 21:58:02 -0300

    modifiquei umas transições - precisa melhorar

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function abrir_pop() {
     let fundo = document.getElementById("esconder");
-    {if (fundo.style.display = "none");
+    if (fundo.style.display = "none") {
         fundo.style.display = 'flex';
     }
 }
 
 function fechar_pop() {
     let fundo = document.getElementById("esconder");
-    {if (fundo.style.display = "flex");
+    if (fundo.style.display = "flex") {
         fundo.style.display = 'none';
     }
 }
+
+function pesquisa_pop() {
+    let input = document.getElementById("pesquisa");
+    if (input.style.display = "none") {
+        input.style.display = 'flex';
+    }
+}
+
+// function pesquisa_dispop() {
+//     let input = document.getElementById("pesquisa");
+//     if (input.style.display = "flex") {
+//         input.style.display = 'none';
+//     }
+// }
